829189753052d2ee3be82c34edfc62eaa77d67d3
Clark19 <clarkgable1@gmail.com>
2021-08-24 23:44:01 +0900

    *리팩토링: DOM API 를 이용한 UI 생성 최소화 작업 == 문자열을 통한 HTML 생성으로 변경 *라우터(화면 전환기) 기초 작업 적용.

--- app.js
+++ app.js
@@ -10,42 +10,49 @@ const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 // ajax 부분 : XMLHttpRequest. 학습 위해 fetch()와 비동기 사용 안함.
 const ajax = new XMLHttpRequest();
-ajax.open('GET', NEWS_URL, false); // 동기 방식: 학습용
-ajax.send();
-// console.log(ajax.response); // 잘 되는지 확인
-
-// 문자열 파싱 부분: 내장 객체 사용해 문자열 배열 형태로 변경
-// newsFeed에 무슨 프로퍼티 있는지는 위 API 문서 링크 참고할 것
-const newsFeed = JSON.parse(ajax.response)
-// console.log(newsFeed)
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
+// 글 내용 구성 부분 - hashchange 핸들러
 window.addEventListener('hashchange', function() {
-  // console.log('해시가 변경됨');
-  const id = location.hash.substr(1);
-
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
-  const title = document.createElement('h1')
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-  console.log(newsContent)
-});
-
-for(let i=0; i<10; i++) {
-  const li = document.createElement('li');
-  const a = document.createElement('a');
-
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-
   /* a.addEventListener('click', function()) 방식은 li 마다 클릭 이벤트 
    리스너 생성 시키는 문제 등이 발생하므로 hashchange 핸들러 방식으로 구현 함 */
-  li.appendChild(a)
-  ul.appendChild(li);
-}
+  // console.log('해시가 변경됨');
+  const id = location.hash.substr(1);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
 
-container.appendChild(ul)
-container.appendChild(content)
+  const title = document.createElement('h1')
+  container.innerHTML = `
+    <h1>${newsContent.title}</h>
+
+    <div>
+      <a href="# ">목록으로</a>
+    </div>
+  `;
+});
+
+// 글 목록 구성 부분
+const newsList = [];
+newsList.push('<ul>');
+for(let i=0; i<10; i++) {
+  const div = document.createElement('div');
+  newsList.push( `
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+  `);
+}
+newsList.push('</ul>');
+
+container.innerHTML = newsList.join('');
+
+function getData(url) {
+  ajax.open('GET', url, false); // 동기 방식: 학습용
+  ajax.send();
+  
+  // 문자열 파싱 부분: 내장 객체 사용해 문자열 배열 형태로 변경
+  // newsFeed에 무슨 프로퍼티 있는지는 위 API 문서 링크 참고할 것
+  return JSON.parse(ajax.response)
+}
