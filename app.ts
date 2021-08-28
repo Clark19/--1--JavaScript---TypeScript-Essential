fa9a7494233284abf218df0042230253daefa646
Clark19 <clarkgable1@gmail.com>
2021-08-28 23:58:06 +0900

    * type alias 를 interface 표기법으로 변경

--- app.ts
+++ app.ts
@@ -5,32 +5,32 @@ hacker news api 사용(사용 신청도 필요 없어 간단)
 */
 
 // 타입 alias
-type Store = {
+interface Store {
   currentPage: number;
   feeds: NewsFeed[];
 }
 
-type News = {
-  id: number;
-  time_ago: string;
-  title: string;
-  url: string;
-  user: string;
-  content: string;
+interface News {
+  readonly id: number;
+  readonly time_ago: string;
+  readonly title: string;
+  readonly url: string;
+  readonly user: string;
+  readonly content: string;
 }
-type NewsFeed = News & {
-  comments_count: number;
-  points: number;
+interface NewsFeed extends News {
+  readonly comments_count: number;
+  readonly points: number;
   read?: boolean;
 }
 
-type NewsDetail = News & {
-  comments: NewsComment[];
+interface NewsDetail extends News {
+  readonly comments: NewsComment[];
 }
 
-type NewsComment = News & {
-  comments: NewsComment[];
-  level: number;
+interface NewsComment extends News {
+  readonly comments: NewsComment[];
+  readonly level: number;
 }
 
 const container: HTMLElement | null = document.getElementById('root');
@@ -64,9 +64,7 @@ function getData<AjaxResponse>(url: string): AjaxResponse {
 
 // 읽은 글 표시하기 위한 프로퍼티 추가
 function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
-  feeds.forEach(feed => {
-    feed.read = false;
-  });
+  feeds.forEach(feed => feed.read = false);
   return feeds;
 }
 
