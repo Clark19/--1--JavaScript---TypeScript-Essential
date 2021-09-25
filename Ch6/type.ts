98c576620997f038593d5ba7bfc9e61a2ff49501
Clark19 <clarkgable1@gmail.com>
2021-09-25 23:59:05 +0900

    Ch06_10. 문법-인터페이스와 타입 별칭 인강 실습

---
+++ Ch6/type.ts
@@ -0,0 +1,107 @@
+// Ch06_10. 문법-인터페이스와 타입 별칭 인강 실습  2021.09.25 타입 정의
+
+export type YesOrNo = 'Y' | 'N';
+// union(|) 키워드 사용.
+export type dyOfWeek = '월' | '화' | '수' | '목' | '금' | '토' | '일';
+export enum dyOfTheWeek {'월', '화', '수', '목', '금', '토', '일'};
+
+export type Name = string;
+export type Email =string;
+export type FooFunction = () => string;
+
+export interface IUser {
+    readonly id: number;
+    readonly name: Name;
+    email: string;
+    receiveInfo: boolean;
+    active: YesOrNo;
+}
+
+export interface IUser {
+    address?: string;
+}
+
+export type TUser = {
+    readonly id: number;
+    readonly name: string;
+    email: Email;
+    receiveInfo: boolean;
+    active: YesOrNo;
+}
+
+// type alias는 인터페이스 처럼 중복(merge)해서 정의 불가능
+// export type TUser = {
+//     address?: string;
+// }
+
+// 상속시 extends 키워드 사용
+export interface IUserProfile extends IUser {
+    profileImage: string;
+    githyub?: string;
+    twitter?: string;
+}
+
+// &  - 상속시 intersection(&) 키워드 사용
+export type TUserProfile = IUser & {
+    profileImage: string;
+    github?: string;
+    twitter?: string;
+}
+
+export interface Color {
+    fontColor: string;
+    strokeColor: string;
+    borderColor: string;
+    backgroundColor: string;
+}
+
+export type Display = {
+    display: 'none' | 'block';
+    visibility: boolean;
+    opacity: number;
+}
+
+export type Geometry = {
+    width: number;
+    height: number;
+    padding: number;
+    margin: number;
+}
+
+export interface IStyle extends Color, Display, Geometry {
+    tagName: string;
+}
+
+export type TStyle = Color & Display & Geometry & {
+    tagName: string;
+}
+
+// 객체에 대한 형식 정의
+export interface IOnlyNumberValueObject {
+    [key:string]: number;
+}
+
+export type TOnlyBooleanValueObject = {
+    [prop:string]: boolean;
+}
+
+// 함수에 대한 시그니쳐(형식) 정의
+export interface IGetApi {
+    (url:string, search?: string): Promise<string>
+}
+
+export type TGetApi = {
+    (url:string, search?: string): Promise<string>
+}
+
+export interface IRect {
+    id: number;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+export interface IRectConstruct {
+    new (x: number, y:number, width:number, height: number): IRect;
+}
---
+++ Ch6/useType.ts
@@ -0,0 +1,67 @@
+// Ch06_10. 문법-인터페이스와 타입 별칭 인강 실습  2021.09.25 . 타입 사용
+import * as type from './type';
+
+const foo: type.FooFunction = function () {
+    return '무의미 함수';
+}
+
+const iUser: type.IUser = {
+    id:1
+    , name: 'myname'
+    , email:'mail@ddd.com'
+    , receiveInfo:false
+    , active: 'Y'
+}
+
+const iUserProfile:type.IUserProfile = {
+    id:1
+    , name:'myname'
+    , email:'bill@mail.com'
+    , receiveInfo: false,
+    active: 'Y',
+    profileImage: 'https://...',
+    github: 'okay',
+}
+
+const IUserProfiles: type.TUser[] = [];
+
+const iStyle: type.IOnlyNumberValueObject = {
+    borderWidth:5,
+    width:300,
+    height:100
+}
+
+const TStyle: type.TOnlyBooleanValueObject = {
+    border:true,
+    visible:false,
+    display:true
+}
+
+const getApi: type.IGetApi = (url, search = '') => {
+    return new Promise(resolve => resolve('OK'));
+}
+
+getApi('./api/users').then(data => console.log(data));
+
+class Rect implements type.IRect {
+    id : number;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+
+    constructor(x: number, y: number, width: number, height: number){
+        this.id = Math.random() * 100000;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+function createDefaultRect(cstor: type.IRectConstruct) {
+    return new cstor(0, 0, 100, 100);
+}
+
+const rect1 = new Rect(0,0,100,20);
+const rect2 = createDefaultRect(Rect);
