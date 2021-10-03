823ff063bcfe903101acde458555bd6600ddfa79
Clark19 <clarkgable1@gmail.com>
2021-10-03 23:33:35 +0900

    Ch06_19. 문법-배열 연산 2 실습

--- Ch6/app.js
+++ Ch6/app.js
@@ -173,3 +173,101 @@ const arr = ['Fire', 'Air', 'Water'];
 console.log(arr.join()); // "Fire,Air,Water" == arr.join(',')
 console.log(arr.join('')); // "FireAirWater"
 
+
+
+/* Ch06_19. 문법-배열 연산 2 실습 2021.10.03
+- 배열 순회
+	- .forEach()
+*/
+const books = [
+  '헨리 6세',
+  '리처드 3세',
+  '실수 연발',
+  '말괄량이 길들이기',
+  '헨리 8세'
+];
+
+books.forEach((book, idx) => {
+  console.log(book, idx);
+});
+
+
+/*
+- .map() : 원본 배열에서 데이터를 입력 받아,  함수가 리턴한 데이터로 새로운 배열을 만들어서 반환하는 메서드
+- 즉, 입력으로 받은 데이터를 함수를 통해서 변경한 다음에, 그 변경된 모양으로 새로운 배열을 만드는 게 바로 map 메서드이다
+- 이렇게 map 함수에 인자로 전달된  함수는 바로, (그 데이터를) 변환하는 코드를 갖고 있다.(우리가 작성 해줘야 하는 코드 임)
+*/
+const bookObject = books.map(book => {
+  return {
+      title: book,
+      author: undefined
+  };
+});
+console.log(bookObjects); // 객체 5개로 이루어진 배열
+
+// 이중 map() 호출 == Method Chaining
+// 이 방식은 중간 리턴 단계의 배열이나 콜백함수를 재사용 할 일이 없을때 이런식으로 사용.
+const ShakespearBooks = books
+  .map(book => ({
+    title: book
+  }))
+  .map(book => ({
+    ...book,
+    author: "William Shakespeare"
+  }));
+  console.log(ShakespearBooks);
+
+// 커링이라는 기법 : 함수를 리턴하는 함수??
+// 함수 안에 함수가 또 있고 그 안쪽의 함수가 리턴하는게 객체 ??? (6장 일급 함수 참고하면 커링 기법 설명하고 있다고 함.
+// 이 방식은 콜백 함수를 재사용 할 때 이런식으로 사용.
+const bookTitleToBookObject = book => ({title: book});
+const makeAuthor = name => book => ({...book, author: name});
+
+const shakespeareBooks2 = books
+  .map(bookTitleToBookObject)
+  .map(makeAuthor("William Shakespeare"));
+console.log(shakespeareTwoBooks);
+
+/*
+- .filter() 메서드: 원본 배열에서 데이터를 입력 받아,  함수가 리턴한 값이 true인 경우만 모아 배열로 리턴.
+- 사용예 : 예를 들어 쇼핑몰 같은 사이트에서 front-end 단에서만 데이터를 걸러서 출력하려고 할때 사용. (서버에서 데이터를 걸러서 주지 않을 때 등)
+  - 물건 목록이 쭉 있고, 위 쪽에 필터 UI들이 있을 때, 예를 들어 '삼성전자' 클릭하면 삼성전자로만 목록이 표시된다거나, TV 누르면 TV만 쭉 나오는걸 FE단에서만 UI 만들어 출력할때 사용 가능. (받은 모든 데이터들이 배열 안에 있을때)
+*/
+const henry = shakespeareTwoBooks.filter(book => book.title.includes("헨리"));
+
+/*
+- .reduce() : 누적/합산 메서드 - 합산을 숫자만 하는게 아니라 객체 merge를 하기도 함.
+- 숫자를 더하는 동작에도 쓰지만, 저렇게 동작하는 메커니즘을 이용해서 굉장히 다양한 응용이 있다.
+*/
+const someNumbers = [10, 5, 3, 14, 56];
+const sumNumbers = someNumbers.reduce((a,b) => a+b, 0);
+/* 함수의 파라미터 (a,b) 에서 b엔 배열 인덱스0 에 해당하는 요소부터 차례대로 들어온다. 10, 5, 3, 14, 56
+    a엔 처음엔 0이 들어오고( => a+b, 0 에서 이 0이 초기값이다.), 그 다음 차례부턴 a+b에 해당하는 값들이 들어온다.
+    즉, 처음 a엔  0이고 그래서 처음엔 리듀스 메서드가 a+b를 리턴하면 그 리턴한 값을 다음번 a 파라미터에 전달한다.
+    그래서 결국 저 코드의 경우엔 배열의 모든 원소에 대한 합이 리턴된다.
+*/
+
+// 배열 안에 여러개의 객체가 존재 할때, 그 객체들을 하나의 객 체로 합치는 데도 사용 가능.
+const someObjects = [
+{border: "none"},
+{fontSize: 24},
+{className: "box sm-box"}
+];
+const obj = someObjects.reduce((a,b) => ({...a, ...b}), {}); // 요소들이 하나로 합쳐진 객체로 됨
+
+
+/*
+- 유사배열을 배열처럼 forEach, map, filter, reduce 메서드들 사용하게 하고 싶을때 Array.from(유사배열) 사용하면 진짜 배열 리턴해줌.
+- Array.from(유사배열).reduce() 형태로 이용 가능:
+*/
+// arguments 는 es5 시대 사용하던 거라, 가독성, 표현력 안좋고, 버그 발생 가능성 있어서 이런게 있구나 정도로만 알면됨. legacy 코드 유지보수/포팅 할 때만 알면 됨.
+// 새로 작성하는 코드는 sumNumbersForES6()에 있는 것처럼 전개 파라미터(...args)를 사용할 것.
+function sumNumbers() {
+  return Array.from(arguments).reduce((a, b) => a+b, 0);
+}
+console.log(sumNumbers(10, 20, 30, 40, 50));
+
+
+function sumNumbersForES6(...args) {
+  return args.reduce((a, b) => a+b, 0);
+}
